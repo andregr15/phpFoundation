0733b3b7ae94bc743bfc791684b45db15e24d827
André Gonçalves Rodrigues <andre.goncalves.rodrigues@gmail.com>
2016-04-07 18:23:35 -0300

    -segunda parte do projeto finalizado

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 
 <?php
-//$rotas = ["Home"=> "index.php", "Contato" => "contato.php", "Empresa" => "empresa.php", "Produtos" => "produto.php", "Serviços" => "servicos.php", "Contato Enviado" => "contatoEnviado.php", "Página Não Encontrada" => "404.php"];
+$rotas = ["Home"=> "index.php", "Contato" => "contato.php", "Empresa" => "empresa.php", "Produtos" => "produto.php", "Serviços" => "servicos.php", "Contato Enviado" => "contatoEnviado.php", "404" => "404.php"];
 $rota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 
 function checarRota(string $rota)
 {
     $nomeArquivo = substr($rota, 1, strlen($rota) -1);
 
-    $nomeArquivo = strpos($nomeArquivo, ".php") ? $nomeArquivo : $nomeArquivo . ".php";
+    if($nomeArquivo == "")
+    {
+        $nomeArquivo = "index.php";
+    }
+
+    $nomeArquivo = strpos($nomeArquivo, ".php") ? $nomeArquivo : $nomeArquivo . ".php"; 
 
     if(!file_exists($nomeArquivo))
     {
-        header("HTTP/1.0 404 Not Found");
-        return "404.php";
+       return "404.php";
     }
     return $nomeArquivo;
 }
@@ -34,7 +38,14 @@ $nomeArquivo = checarRota($rota['path']);
             {   
                 echo "<div class=\"well\">	<center>		<h1>Home</h1>	</center></div>";
             }
-            require_once($nomeArquivo); 
+
+            require_once($nomeArquivo);     
+            if(str_replace(".php", "", $nomeArquivo) == "404")
+            {
+                //gerando erro no php, peguntei na parte de dúvidas e não obtive resposata até o momento 07/04/2016 18:22
+                header("HTTP/1.0 404 Not Found");
+            }
+
             ?>
 	</body>
 	
